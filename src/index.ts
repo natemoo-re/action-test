df317b4698ed9baaf8ec10126226a798214fce65
Nate Moore <nate@astro.build>
2024-01-30 21:55:08 -0600

    build

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import * as core from '@actions/core'
-import resolve from 'resolve-package-path'
 
 async function run(): Promise<void> {
   try {
-    const path = resolve('astro', process.cwd())
-    console.log({ path });
-    core.setOutput('value', path)
+    console.log(process.env)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
